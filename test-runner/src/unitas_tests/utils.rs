6ed87a7cdefb708cd1ad2fa97d5557d3e4e86072
Eddio0141 <eddio0141@gmail.com>
2024-08-29 00:04:44 +0100

    show left and right values in assert_eq

--- test-runner/src/unitas_tests/utils.rs
+++ test-runner/src/unitas_tests/utils.rs
@@ -7,10 +7,17 @@ use crate::symbols;
 pub fn assert_eq<T, F>(test_name: &str, left: T, right: T, message: F) -> bool
 where
     F: FnOnce() -> String,
-    T: std::cmp::PartialEq,
+    T: std::cmp::PartialEq + std::fmt::Debug,
 {
     let success = left == right;
-    let err = if success { None } else { Some(message()) };
+    let err = if success {
+        None
+    } else {
+        Some(format!(
+            "{}, left: `{left:?}`, right: `{right:?}`",
+            message()
+        ))
+    };
 
     print_test_result(test_name, success, err);
 
