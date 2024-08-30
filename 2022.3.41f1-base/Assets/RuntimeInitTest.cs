fdbd6bf843e34bfff4e2c04c7171c0a5a96779ae
Eddio0141 <eddio0141@gmail.com>
2024-08-31 00:31:24 +0100

    test runtime init

--- 2022.3.41f1-base/Assets/RuntimeInitTest.cs
+++ 2022.3.41f1-base/Assets/RuntimeInitTest.cs
@@ -1,31 +1,31 @@
 using UnityEngine;
 
-internal static class RuntimeInit
+internal static class RuntimeInitTest
 {
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
     private static void BeforeSplashScreen()
     {
         Debug.Log("BeforeSplashScreen");
     }
-    
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
     private static void SubsystemRegistration()
     {
         Debug.Log("SubsystemRegistration");
     }
-    
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void BeforeSceneLoad()
     {
         Debug.Log("BeforeSceneLoad");
     }
-    
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
     private static void AfterAssembliesLoaded()
     {
         Debug.Log("AfterAssembliesLoaded");
     }
-    
+
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
     private static void AfterSceneLoad()
     {
