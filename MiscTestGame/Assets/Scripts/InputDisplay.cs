6dc7d188462d5f206babe5f651619c4942ec685a
Eddio0141 <eddio0141@gmail.com>
2024-11-28 22:53:32 +0000

    scene testing stuff

--- MiscTestGame/Assets/Scripts/InputDisplay.cs
+++ MiscTestGame/Assets/Scripts/InputDisplay.cs
@@ -4,6 +4,7 @@ using System.Text;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 [RequireComponent(typeof(TextMeshProUGUI), typeof(RectTransform))]
 public class InputDisplay : MonoBehaviour
@@ -15,6 +16,8 @@ public class InputDisplay : MonoBehaviour
 
     private void Awake()
     {
+        Debug.Log("Awake");
+
         _text = GetComponent<TextMeshProUGUI>();
         if (_text == null)
         {
@@ -39,7 +42,25 @@ public class InputDisplay : MonoBehaviour
         var rect = _parentRectTransform.rect;
         _textRectTransform.sizeDelta = new(rect.width, rect.height);
 
-        StartCoroutine(CheckRes());
+        //StartCoroutine(CheckRes());
+
+        SceneManager.activeSceneChanged += (before, after) =>
+            Debug.Log($"activeSceneChanged, before: {before.name}, after: {after.name}, frame: {Time.frameCount}");
+        SceneManager.sceneLoaded += (scene, mode) =>
+            Debug.Log($"sceneLoaded, {scene.name}, {mode}, frame: {Time.frameCount}");
+        SceneManager.sceneUnloaded += scene => Debug.Log($"sceneUnloaded, {scene.name}, frame: {Time.frameCount}");
+
+        StartCoroutine(EndOfFrame());
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("OnEnable");
+    }
+
+    private void Start()
+    {
+        Debug.Log("Start");
     }
 
     private IEnumerator CheckRes()
@@ -57,11 +78,11 @@ public class InputDisplay : MonoBehaviour
     }
 
     private bool _smallRes;
+    private AsyncOperation emptyOp;
+    private AsyncOperation unloadOp;
 
     private void Update()
     {
-        if (_text == null) return;
-
         if (Input.GetKeyDown(KeyCode.J))
         {
             _smallRes = !_smallRes;
@@ -75,6 +96,56 @@ public class InputDisplay : MonoBehaviour
                 Screen.SetResolution(1920, 1080, false);
             }
         }
+
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            emptyOp = SceneManager.LoadSceneAsync("Empty", LoadSceneMode.Additive)!;
+            Debug.Log(
+                $"sceneCount: {SceneManager.sceneCount}, loadedSceneCount: {SceneManager.loadedSceneCount}, {emptyOp.progress}, {Time.frameCount}");
+            // emptyOp.allowSceneActivation = false;
+            // SceneManager.LoadScene("Empty", LoadSceneMode.Additive);
+            // var op = SceneManager.LoadSceneAsync("Empty", LoadSceneMode.Additive);
+            // op.allowSceneActivation = false;
+            // op.completed += _ => { Debug.Log("a"); };
+            Debug.Log($"progress: {emptyOp.progress}");
+            emptyOp.completed += _ =>
+            {
+                Debug.Log(
+                    $"loaded Empty async at frame {Time.frameCount}, {Time.captureFramerate}, {Time.renderedFrameCount}, scene count: {SceneManager.sceneCount}, {SceneManager.loadedSceneCount}");
+            };
+        }
+
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            // Debug.Log(
+            //     $"allow scene at frame: {Time.frameCount}, {Time.captureFramerate}, {Time.renderedFrameCount}, scene count: {SceneManager.sceneCount}, {SceneManager.loadedSceneCount}");
+            // _emptyOp.allowSceneActivation = true;
+            Debug.Log("Empty load non async");
+            SceneManager.LoadScene("Empty", LoadSceneMode.Additive);
+            Debug.Log($"allow scene after scene count: {SceneManager.sceneCount}, {SceneManager.loadedSceneCount}");
+        }
+
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            Debug.Log(
+                $"load sync scene at frame: {Time.frameCount}, {Time.captureFramerate}, {Time.renderedFrameCount}, scene count: {SceneManager.sceneCount}, {SceneManager.loadedSceneCount}");
+            SceneManager.LoadScene("Empty");
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            // Debug.Log($"Empty status {_emptyOp.allowSceneActivation}, {_emptyOp.isDone}, {_emptyOp.progress}");
+            Debug.Log($"sceneCount: {SceneManager.sceneCount}, loadedSceneCount: {SceneManager.loadedSceneCount}");
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            unloadOp = SceneManager.UnloadSceneAsync("Empty");
+            if (unloadOp != null)
+                unloadOp.completed += op => { Debug.Log($"Empty unload async, {op.progress}, {op.isDone}"); };
+        }
+
+        if (_text == null) return;
 
         var builder = new StringBuilder();
 
@@ -98,5 +169,14 @@ public class InputDisplay : MonoBehaviour
             : $"Keyboard: {string.Join(", ", keyboard.allKeys.Where(k => k.isPressed).Select(k => k.keyCode.ToString()))}");
 
         _text.text = builder.ToString();
+    }
+
+    private IEnumerator EndOfFrame()
+    {
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            _ = 1;
+        }
     }
 }
