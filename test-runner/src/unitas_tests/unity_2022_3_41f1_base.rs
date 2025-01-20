233b0270d823ef8c5c19f7376f8a6a34299c2663
Eddio0141 <eddio0141@gmail.com>
2025-01-20 21:45:32 +0000

    more proper testing with unitas updates

--- test-runner/src/unitas_tests/unity_2022_3_41f1_base.rs
+++ test-runner/src/unitas_tests/unity_2022_3_41f1_base.rs
@@ -113,6 +113,141 @@ fn test(ctx: &mut TestCtx, mut args: TestArgs) -> Result<()> {
         "didn't match click count of 5",
     );
 
+    let frame_count = 100u8;
+
+    // unitas updates
+
+    // fixed update: 0.02
+    // ---
+    // update: 0.02
+    // late update: 0.02 - not included here
+    // end of frame: 0.02
+    // last update: 0.02
+    // ---
+    // update: 0.03
+    // late update: 0.03 - not included here
+    // end of frame: 0.03
+    // last update: 0.03
+
+    stream.send(&format!(
+        r#"time = traverse('UnityEngine.Time')
+service('ITimeWrapper').capture_frame_time = 0.01
+
+wait_for_fixed_update = true
+wait_for_last = true
+fixed_update_count = 0
+update_count = 0
+end_of_frame_count = 0
+last_update_count = 0
+
+printed_results = 0
+
+local reverse_invoker = service("IPatchReverseInvoker")
+local fixed_time = traverse("UnityEngine.Time").property("fixedTime")
+
+patch("UniTAS.Patcher.Implementations.UnityEvents.UnityEvents.InvokeFixedUpdate", function(this)
+
+    reverse_invoker.invoking = true
+    local fixed_time = fixed_time.GetValue()
+    reverse_invoker.invoking = false
+    if traverse(this).field("_prevFixedTime").GetValue() == fixed_time then
+        return
+    end
+
+    wait_for_fixed_update = false
+
+    if wait_for_last then
+        return
+    end
+
+    if update_count >= {frame_count} then
+        if printed_results < 2 then
+            print(fixed_update_count)
+            print(update_count)
+            print(end_of_frame_count)
+            print(last_update_count)
+            printed_results = printed_results + 1
+
+            -- test #2 init
+            if printed_results == 1 then
+                time.property('maximumDeltaTime').set_value(0.3333333)
+                time.property('fixedDeltaTime').set_value(0.02)
+                time.property('timeScale').set_value(1)
+                service('ITimeWrapper').capture_frame_time = 0.04
+                wait_for_fixed_update = true
+                wait_for_last = true
+
+                fixed_update_count = 0
+                update_count = 0
+                end_of_frame_count = 0
+                last_update_count = 0
+            end
+        else
+            return
+        end
+    end
+
+    fixed_update_count = fixed_update_count + 1
+end, "method")
+patch("UniTAS.Patcher.Implementations.UnityEvents.UnityEvents.InvokeUpdate", function(this)
+    if wait_for_last or wait_for_fixed_update or update_count >= {frame_count} or traverse(this).field("_updated").get_value() then
+        return
+    end
+
+    update_count = update_count + 1
+end, "method")
+patch("UniTAS.Patcher.Implementations.UnityEvents.UnityEvents.InvokeEndOfFrame", function(this)
+    if wait_for_last or wait_for_fixed_update or update_count >= {frame_count} or traverse(this).field("_endOfFrameUpdated").get_value() then
+        return
+    end
+
+    end_of_frame_count = end_of_frame_count + 1
+end, "method")
+patch("UniTAS.Patcher.Implementations.UnityEvents.UnityEvents.InvokeLastUpdate", function()
+    if wait_for_fixed_update or update_count >= {frame_count} then
+        return
+    end
+
+    last_update_count = last_update_count + 1
+    wait_for_last = false
+end, "method")
+"#
+    ))?;
+
+    // ignore messages
+    for _ in 0..8 {
+        stream.receive()?;
+    }
+
+    // fixed_update_count
+    // update_count
+    // end_of_frame_count
+    // last_update_count
+    ctx.assert_eq(
+        &(frame_count / 2).to_string(),
+        &stream.receive()?,
+        "unitas updates: fixed update count",
+        "mismatch in update count",
+    );
+    ctx.assert_eq(
+        &frame_count.to_string(),
+        &stream.receive()?,
+        "unitas updates: update count",
+        "mismatch in update count",
+    );
+    ctx.assert_eq(
+        &(frame_count - 1).to_string(),
+        &stream.receive()?,
+        "unitas updates: end of frame count",
+        "mismatch in update count",
+    );
+    ctx.assert_eq(
+        &frame_count.to_string(),
+        &stream.receive()?,
+        "unitas updates: last update count",
+        "mismatch in update count",
+    );
+
     // multiple fixed updates in a row
     // in this case, this pattern is made
 
@@ -126,74 +261,34 @@ fn test(ctx: &mut TestCtx, mut args: TestArgs) -> Result<()> {
     // f: 0.10
     // f: 0.12
     // u: 0.12
-    let max_fixed_update_count = 4u8;
 
-    // TODO: create standard way to easily read things
-    // TODO: on unity side, create a "results" class containing all results of the test
-    stream.send(&format!(
-        r#"time = traverse('UnityEngine.Time')
-time.property('maximumDeltaTime').set_value(0.3333333)
-time.property('fixedDeltaTime').set_value(0.02)
-time.property('timeScale').set_value(1)
-service('ITimeWrapper').capture_frame_time = 0.04
-
-wait_for_update = true
-fixed_update_count = 0
-update_count = 0
-
-printed_results = false
-
-local reverse_invoker = service("IPatchReverseInvoker")
-local fixed_time = traverse("UnityEngine.Time").property("fixedTime")
-
-patch("UniTAS.Patcher.Implementations.UnityEvents.UnityEvents.InvokeFixedUpdate", function(this)
-    reverse_invoker.invoking = true;
-    local time = fixed_time.GetValue()
-    reverse_invoker.invoking = false;
-
-    if traverse(this).field("_prevFixedTime").GetValue() != time or wait_for_update then
-        return
-    end
-
-    if fixed_update_count >= {max_fixed_update_count} then
-        if not printed_results then
-            print(fixed_update_count)
-            print(update_count)
-            printed_results = true
-        end
-        return
-    end
-
-    fixed_update_count = fixed_update_count + 1
-end, "method")
-patch("UniTAS.Patcher.Implementations.UnityEvents.UnityEvents.InvokeUpdate", function(this)
-    if traverse(this).field("_updated").get_value() or fixed_update_count >= {max_fixed_update_count} then
-        return
-    end
-
-    wait_for_update = false
-    update_count = update_count + 1
-end, "method")
-"#
-    ))?;
-
-    // ignore messages
-    for _ in 0..4 {
-        stream.receive()?;
-    }
-
+    // fixed_update_count
+    // update_count
+    // end_of_frame_count
+    // last_update_count
     ctx.assert_eq(
-        &max_fixed_update_count.to_string(),
+        &(frame_count * 2 + 1).to_string(),
         &stream.receive()?,
-        "multiple fixed update: fixed update count",
-        "mismatch in FixedUpdate count",
+        "unitas updates: fixed update count",
+        "mismatch in update count",
     );
-
     ctx.assert_eq(
-        "2",
+        &frame_count.to_string(),
         &stream.receive()?,
-        "multiple fixed update: update count",
-        "mismatch in Update count",
+        "unitas updates: update count",
+        "mismatch in update count",
+    );
+    ctx.assert_eq(
+        &(frame_count - 1).to_string(),
+        &stream.receive()?,
+        "unitas updates: end of frame count",
+        "mismatch in update count",
+    );
+    ctx.assert_eq(
+        &frame_count.to_string(),
+        &stream.receive()?,
+        "unitas updates: last update count",
+        "mismatch in update count",
     );
 
     stream.send("full_access(true)")?;
