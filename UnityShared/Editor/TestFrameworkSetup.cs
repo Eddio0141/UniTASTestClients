77895c362b6f40f3366bdb91d6f0dcf57c6db90e
Eddio0141 <eddio0141@gmail.com>
2025-05-11 22:11:06 +0100

    partially working linking via unity test version

--- UnityShared/Editor/TestFrameworkSetup.cs
+++ UnityShared/Editor/TestFrameworkSetup.cs
@@ -18,21 +18,51 @@ namespace Editor
         {
             Debug.Log("Loading UniTAS testing framework");
 
-            SetupFilesAndDirs();
+            InitDirs();
+            var (sharedScriptsDir, sharedEditorDir, testsDir) = GetRepoDirs();
+            LinkRunnerFiles(sharedScriptsDir, sharedEditorDir);
+            LinkTests(testsDir);
             var testObj = InitTestScene();
             SetupTestScene(testObj);
 
             Debug.Log("Finished loading UniTAS testing framework");
         }
 
-        private static void SetupFilesAndDirs()
-        {
-            const string scriptsDir = "Assets/Scripts";
-            const string editorDir = "Assets/Editor";
+        private const string ScriptsDir = "Assets/Scripts";
 
-            // not generating editor dir, how else is this script running then?
+        private static ( string sharedScriptsDir, string sharedEditorDir, string testsDir) GetRepoDirs()
+        {
+            var repoDir = Directory.GetCurrentDirectory();
+            while (Path.GetFileName(repoDir) != "UniTASTestClients")
+            {
+                repoDir = Directory.GetParent(repoDir)?.FullName;
+                if (repoDir != null) continue;
+                throw new Exception("Failed to find repository base directory, failed file setup");
+            }
+
+            var sharedDir = Path.Combine(repoDir, "UnityShared");
+            AssertDirExists(sharedDir);
+            var sharedScriptsDir = Path.Combine(sharedDir, "Scripts");
+            AssertDirExists(sharedScriptsDir);
+            var sharedEditorDir = Path.Combine(sharedDir, "Editor");
+            AssertDirExists(sharedEditorDir);
+            var testsDir = Path.Combine(sharedDir, "Tests");
+            AssertDirExists(testsDir);
+
+            return (sharedScriptsDir, sharedEditorDir, testsDir);
+        }
+
+        private static void AssertDirExists(string dir)
+        {
+            UnityEngine.Assertions.Assert.IsTrue(Directory.Exists(dir));
+        }
+
+        private static void InitDirs()
+        {
             var createPaths = new[]
-                { TestFrameworkRuntime.SceneAssetPath, TestFrameworkRuntime.PrefabAssetPath, scriptsDir };
+            {
+                TestFrameworkRuntime.SceneAssetPath, TestFrameworkRuntime.PrefabAssetPath, ScriptsDir
+            };
             foreach (var path in createPaths)
             {
                 if (!Directory.Exists(path))
@@ -40,27 +70,16 @@ namespace Editor
                     Directory.CreateDirectory(path);
                 }
             }
+        }
+
+        private static void LinkRunnerFiles(string sharedScriptsDir, string sharedEditorDir)
+        {
+            const string editorDir = "Assets/Editor";
 
             // TODO: figure out which unity version didn't work with symlinks
 
-            var repoDir = Directory.GetCurrentDirectory();
-            while (Path.GetFileName(repoDir) != "UniTASTestClients")
-            {
-                repoDir = Directory.GetParent(repoDir)?.FullName;
-                if (repoDir != null) continue;
-                Debug.LogError("Failed to find repository base directory, failed file setup");
-                return;
-            }
-
-            var sharedDir = Path.Combine(repoDir, "UnityShared");
-            UnityEngine.Assertions.Assert.IsTrue(Directory.Exists(sharedDir));
-            var sharedScriptsDir = Path.Combine(sharedDir, "Scripts");
-            UnityEngine.Assertions.Assert.IsTrue(Directory.Exists(sharedScriptsDir));
-            var sharedEditorDir = Path.Combine(sharedDir, "Editor");
-
             // link everything
-            var links = new[] { (sharedScriptsDir, scriptsDir), (sharedEditorDir, editorDir) };
-
+            var links = new[] { (sharedScriptsDir, ScriptsDir), (sharedEditorDir, editorDir) };
             foreach (var (sourceDir, destDir) in links)
             {
                 foreach (var sourceFile in Directory.GetFiles(sourceDir, "*.cs", SearchOption.TopDirectoryOnly))
@@ -76,6 +95,81 @@ namespace Editor
             }
         }
 
+        private static void LinkTests(string testsDir)
+        {
+            foreach (var sourceFile in Directory.GetFiles(testsDir, "*.cs", SearchOption.TopDirectoryOnly))
+            {
+                var fileNameNoExt = Path.GetFileNameWithoutExtension(sourceFile);
+                if (!MatchesVersion(fileNameNoExt))
+                {
+                    continue;
+                }
+
+                Debug.Log($"found matching test file `{fileNameNoExt}`");
+                var destFile = Path.Combine(ScriptsDir, Path.GetFileName(sourceFile));
+                if (File.Exists(destFile))
+                {
+                    File.Delete(destFile);
+                }
+
+                RelativeSymlinkFile(sourceFile, destFile);
+            }
+        }
+
+        private const string TinySep = "_";
+        private const string BigSep = "__";
+
+        private static bool MatchesVersion(string testName)
+        {
+            var exampleTestName = $"`Category{BigSep}2022{TinySep}3{TinySep}41{BigSep}2023{TinySep}3`";
+
+            var versionStartIdx = testName.IndexOf(BigSep, StringComparison.InvariantCulture);
+            if (versionStartIdx == -1)
+            {
+                throw new InvalidOperationException(
+                    $"test name `{testName}` is formatted wrong, missing initial `{BigSep}` before stating" +
+                    " minimum unity version like so: " + exampleTestName);
+            }
+
+            if (versionStartIdx == 0)
+            {
+                Debug.LogWarning($"test name `{testName}` has got no category prefixed in the name like so: " +
+                                 exampleTestName);
+            }
+
+            var fullVersionRaw = testName[(versionStartIdx + BigSep.Length)..];
+            var versionSepIdx = fullVersionRaw.IndexOf(BigSep, StringComparison.InvariantCulture);
+            if (versionSepIdx == -1)
+            {
+                throw new InvalidOperationException(
+                    $"test name `{testName}` doesn't have a max version defined for the test, only the min version" +
+                    "you need to add the maximum inclusive version like so: " + exampleTestName);
+            }
+
+            var versionMinRaw = fullVersionRaw[..versionSepIdx];
+            var versionMaxRaw = fullVersionRaw[(versionSepIdx + BigSep.Length)..];
+
+            var versionMin = GetVersionFromRaw(versionMinRaw);
+            var versionMax = GetVersionFromRaw(versionMaxRaw);
+            
+            var currentVersion = Application.version;
+        }
+
+        private static IEnumerable<int> GetVersionFromRaw(string rawVersion)
+        {
+            var split = rawVersion.Split(TinySep);
+            return split.Select(v =>
+            {
+                if (int.TryParse(v, out var success))
+                {
+                    return success;
+                }
+
+                throw new InvalidOperationException(
+                    $"invalid version: `{rawVersion}`, make sure each version number is separated by `{TinySep}`");
+            });
+        }
+
         private static void RelativeSymlinkFile(string source, string target)
         {
             var targetWorking = Directory.GetParent(target)?.FullName;
@@ -85,7 +179,6 @@ namespace Editor
             }
 
             // find out how much we need to go back to reach source dir
-
             var sourceRel = string.Empty;
             while (!source.StartsWith(targetWorking))
             {
@@ -101,7 +194,6 @@ namespace Editor
 
             // now push rest of the path
             source = sourceRel + source[(targetWorking.Length + 1)..];
-
             var plat = Environment.OSVersion.Platform;
             var success = plat switch
             {
@@ -110,7 +202,6 @@ namespace Editor
                     CreateSymbolicLink(target, source, SymbolicLink.File),
                 _ => throw new NotImplementedException($"symlink operation not implemented for platform {plat}")
             };
-
             if (!success)
             {
                 throw new Exception($"symlink failed: error code {Marshal.GetLastWin32Error()}");
@@ -135,10 +226,8 @@ namespace Editor
             var scene = AssetDatabase.AssetPathExists(TestFrameworkRuntime.TestingScenePath)
                 ? EditorSceneManager.OpenScene(TestFrameworkRuntime.TestingScenePath, OpenSceneMode.Single)
                 : EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
-
             const string testObjName = "Tests";
             const string eventHooksObjName = "EventHooks";
-
             var testObj = Object.FindObjectsByType<GameObject>(FindObjectsSortMode.None)
                 .FirstOrDefault(o => o.name == testObjName);
             if (testObj == null)
@@ -167,13 +256,10 @@ namespace Editor
                 saveScene = true;
             }
 
-            if (saveScene)
-                EditorSceneManager.SaveScene(scene, TestFrameworkRuntime.TestingScenePath);
-
+            if (saveScene) EditorSceneManager.SaveScene(scene, TestFrameworkRuntime.TestingScenePath);
             var buildScenes = EditorBuildSettings.scenes.ToList();
             buildScenes.Add(new EditorBuildSettingsScene(TestFrameworkRuntime.TestingScenePath, true));
             EditorBuildSettings.scenes = buildScenes.ToArray();
-
             return testObj;
         }
 
@@ -183,24 +269,21 @@ namespace Editor
             {
                 var type = monoBeh.GetType();
                 var testMethods = TestFrameworkRuntime.GetTestFuncs(type);
-
                 var invalidTest = false;
                 var hasTests = false;
                 foreach (var testMethod in testMethods)
                 {
                     hasTests = true;
                     if (testMethod.ReturnType == typeof(void) ||
-                        testMethod.ReturnType == typeof(IEnumerator<TestYield>)) continue;
+                        testMethod.ReturnType == typeof(IEnumerator<TestYield>))
+                        continue;
                     Debug.LogError("Test return type must be void or IEnumerable<TestYield>");
                     invalidTest = true;
                     break;
                 }
 
-                if (invalidTest || !hasTests)
-                    continue;
-
-                var injectFields = type
-                    .GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+                if (invalidTest || !hasTests) continue;
+                var injectFields = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
                     .Select(f => (f.Name, f.GetCustomAttribute<TestInjectAttribute>(true), f.FieldType))
                     .Where(tuple => tuple.Item2 != null);
                 var prop = new SerializedObject(monoBeh);
